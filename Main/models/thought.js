2ffc7706361c321f3eaec6cf6a9e8ce707c4e596
Nathaniel Motley <nmotley92@gmail.com>
2023-02-27 18:23:14 -0600

    adds models for user, thoughts, and reactions

---
+++ Main/models/thought.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Reaction',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// get total count of reactions on retrieval
+ThoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
+// format the timestamp on retrieval
+const dateFormat = (timestamp) => {
+  return new Date(timestamp).toISOString();
+};
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
